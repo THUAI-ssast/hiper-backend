e2a6e9c3ecf9765de950c77ffeb4c2da213e4a10
he ting <2975587905@qq.com>
2023-11-09 17:49:11 +0800

    user register

--- web/api/api.go
+++ web/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"hiper-backend/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,7 @@ func ApiListenHttp() {
 	r := gin.Default()
 
 	// TODO: routes
-	r.GET("/users/request-verification-code", func(c *gin.Context) {
+	r.POST("/users/request-verification-code", func(c *gin.Context) {
 		var jsonGetV GetRequestVerificationCode
 		if err := c.ShouldBindJSON(&jsonGetV); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -33,7 +34,8 @@ func ApiListenHttp() {
 			})
 		}
 	})
-	r.GET("/users", func(c *gin.Context) {
+
+	r.POST("/users", func(c *gin.Context) {
 		var jsonGetR GetRegister
 		if err := c.ShouldBindJSON(&jsonGetR); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -42,6 +44,7 @@ func ApiListenHttp() {
 		email := jsonGetR.Email
 		code := jsonGetR.Code
 		password := jsonGetR.Password
+		username := jsonGetR.Username
 		if !code_match(code, email) {
 			c.JSON(422, gin.H{
 				"errors": []gin.H{
@@ -60,7 +63,7 @@ func ApiListenHttp() {
 					},
 				},
 			})
-		} else if !email_not_exist(email) {
+		} else if email_exist_codegiven(email) {
 			c.JSON(422, gin.H{
 				"errors": []gin.H{
 					{
@@ -70,7 +73,7 @@ func ApiListenHttp() {
 				},
 			})
 		} else {
-			userId := set_user(email, password)
+			userId := set_user(email, password, username)
 			if userId == -1 {
 				c.JSON(422, gin.H{
 					"errors": []gin.H{
@@ -87,7 +90,164 @@ func ApiListenHttp() {
 				})
 			}
 		}
-
 	})
+
+	r.POST("/users/reset-email", func(c *gin.Context) {
+		var jsonGetRE GetResetEmail
+		if err := c.ShouldBindJSON(&jsonGetRE); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		email := jsonGetRE.Email
+		code := jsonGetRE.Code
+		newEmail := jsonGetRE.NewEmail
+		if !email_exist_codegiven(email) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "email",
+					},
+				},
+			})
+		} else if !verify_email(newEmail) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "new_email",
+					},
+				},
+			})
+		} else if !code_match(code, email) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "verification_code",
+					},
+				},
+			})
+		} else {
+			update_email(email, newEmail)
+			c.JSON(200, gin.H{})
+		}
+	})
+
+	r.POST("/users/reset-password", func(c *gin.Context) {
+		var jsonGetRP GetResetPassword
+		if err := c.ShouldBindJSON(&jsonGetRP); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		email := jsonGetRP.Email
+		code := jsonGetRP.Code
+		password := jsonGetRP.Password
+		if !email_exist_codegiven(email) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "email",
+					},
+				},
+			})
+		} else if !verify_password(password) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "password",
+					},
+				},
+			})
+		} else if !code_match(code, email) {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "invalid",
+						"field": "verification_code",
+					},
+				},
+			})
+		} else {
+			update_password(email, password)
+			c.JSON(200, gin.H{})
+		}
+	})
+
+	r.POST("/users/login", func(c *gin.Context) {
+		var jsonGetL GetLogin
+		if err := c.ShouldBindJSON(&jsonGetL); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		email := jsonGetL.Email
+		username := jsonGetL.Username
+		password := jsonGetL.Password
+		if username == "" && email == "" {
+			c.JSON(422, gin.H{
+				"errors": []gin.H{
+					{
+						"code":  "missing_field",
+						"field": "email and username",
+					},
+				},
+			})
+		} else if username != "" {
+			if !username_exist_user(username) {
+				c.JSON(422, gin.H{
+					"errors": []gin.H{
+						{
+							"code":  "invalid",
+							"field": "username",
+						},
+					},
+				})
+			} else if !password_match_username(username, password) {
+				c.JSON(422, gin.H{
+					"errors": []gin.H{
+						{
+							"code":  "invalid",
+							"field": "password",
+						},
+					},
+				})
+			} else {
+				userId := get_userId_username(username)
+				token, _ := utils.GenToken((int64)(userId))
+				c.JSON(200, gin.H{
+					"access_token": token,
+				})
+			}
+		} else {
+			if !email_exist_user(email) {
+				c.JSON(422, gin.H{
+					"errors": []gin.H{
+						{
+							"code":  "invalid",
+							"field": "email",
+						},
+					},
+				})
+			} else if !password_match_email(email, password) {
+				c.JSON(422, gin.H{
+					"errors": []gin.H{
+						{
+							"code":  "invalid",
+							"field": "password",
+						},
+					},
+				})
+			} else {
+				userId := get_userId_email(email)
+				token, _ := utils.GenToken((int64)(userId))
+				c.JSON(200, gin.H{
+					"access_token": token,
+				})
+			}
+		}
+	})
+
 	r.Run(":8080")
 }
--- web/api/user.go
+++ web/api/user.go
@@ -2,14 +2,15 @@ package api
 
 import (
 	"fmt"
+	"hiper-backend/config"
+	"hiper-backend/mail"
+
+	// "hiper-backend/utils"
 	"math/rand"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
-
-	"hiper-backend/config"
-	"hiper-backend/mail"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -21,6 +22,25 @@ type GetRequestVerificationCode struct {
 type GetRegister struct {
 	Email    string `json:"email" binding:"required"`
 	Code     string `json:"verification_code" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Username string `json:"username" binding:"required"`
+}
+
+type GetResetEmail struct {
+	Email    string `json:"email" binding:"required"`
+	Code     string `json:"verification_code" binding:"required"`
+	NewEmail string `json:"new_email" binding:"required"`
+}
+
+type GetResetPassword struct {
+	Email    string `json:"email" binding:"required"`
+	Code     string `json:"verification_code" binding:"required"`
+	Password string `json:"new_password" binding:"required"`
+}
+
+type GetLogin struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
 	Password string `json:"password" binding:"required"`
 }
 
@@ -46,43 +66,70 @@ func send_email(email string) {
 	// 生成随机验证码
 	code := GenValidateCode(6)
 	timeUnix := time.Now().Unix()
-	sqlStr := fmt.Sprintf(`insert into codegiven(email,code,time) values("%s","%s",%d)`, email, code, timeUnix) //sql语句
-	ret, err := config.Db.Exec(sqlStr)                                                                          //执行sql语句
-	if err != nil {
-		fmt.Printf("insert failed,err:%v\n", err)
-		return
-	}
-	//如果是插入数据的操作，能够拿到插入数据的id
-	_, err = ret.LastInsertId()
-	if err != nil {
-		fmt.Printf("get id failed,err:%v\n", err)
-		return
+	sqlStr := fmt.Sprintf("select * from codegiven where email='%s';", email)
+	//2.执行
+	_, ok := config.Query(sqlStr) //从连接池里取一个连接出来去数据库查询记录
+	if !ok {
+		sqlStr = fmt.Sprintf(`insert into codegiven(email,code,time) values("%s","%s",%d)`, email, code, timeUnix) //sql语句
+		ret, err := config.Db.Exec(sqlStr)                                                                         //执行sql语句
+		if err != nil {
+			fmt.Printf("insert failed,err:%v\n", err)
+			return
+		}
+		//如果是插入数据的操作，能够拿到插入数据的id
+		_, err = ret.LastInsertId()
+		if err != nil {
+			fmt.Printf("get id failed,err:%v\n", err)
+			return
+		}
+	} else {
+		sqlStr := `update codegiven set code=?,time=? where email=?`
+		_, err := config.Db.Exec(sqlStr, code, timeUnix, email)
+		if err != nil {
+			fmt.Printf("update failed ,err:%v\n", err)
+			return
+		}
 	}
 	mail.MailTo(email, code)
 }
 
 func code_match(code string, email string) bool {
 	//1.查询记录的sql语句
-	sqlStr := fmt.Sprintf("select * from codegiven where email=%s;", email)
+	sqlStr := fmt.Sprintf("select * from codegiven where email='%s';", email)
 	//2.执行
 	rst, ok := config.Query(sqlStr) //从连接池里取一个连接出来去数据库查询记录
 	if ok {
 		timeUnix := time.Now().Unix()
 		timeThen, _ := strconv.ParseInt(rst[0]["time"], 10, 64)
-		return code == rst[0]["code"] && timeUnix-timeThen < 300
+		return code == rst[0]["code"] && timeUnix-timeThen < 30000 //TODO:change to 300
 	} else {
 		return false
 	}
 }
 
-func set_user(email string, password string) int {
+func set_user(email string, password string, username string) int {
 	//删除数据库db中的数据
 	sqlStr := `delete from codegiven where email=?`
 	_, err := config.Db.Exec(sqlStr, email)
 	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
 		return -1
 	}
-	return 1 //TODO: set user in database
+
+	sqlStr = fmt.Sprintf(`insert into user(username,email,password) values("%s","%s","%s")`, username, email, password) //sql语句
+	ret, err := config.Db.Exec(sqlStr)                                                                                  //执行sql语句
+	if err != nil {
+		fmt.Printf("insert failed,err:%v\n", err)
+		return -1
+	}
+	//如果是插入数据的操作，能够拿到插入数据的id
+	id, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get id failed,err:%v\n", err)
+		return -1
+	}
+
+	return (int)(id)
 }
 
 func verify_password(password string) bool {
@@ -92,10 +139,52 @@ func verify_password(password string) bool {
 	return m
 }
 
-func email_not_exist(email string) bool {
+func email_exist_codegiven(email string) bool {
 	//1.查询记录的sql语句
-	sqlStr := fmt.Sprintf("select * from user where email=%s;", email)
+	sqlStr := fmt.Sprintf("select * from user where email='%s';", email)
 	//2.执行
 	_, ok := config.Query(sqlStr) //从连接池里取一个连接出来去数据库查询记录
-	return !ok
+	return ok
+}
+
+func update_email(email string, newEmail string) {
+	sqlStr := `update user set email=? where email=?`
+	_, err := config.Db.Exec(sqlStr, newEmail, email)
+	if err != nil {
+		fmt.Printf("update failed ,err:%v\n", err)
+		return
+	}
+}
+
+func update_password(email string, password string) {
+	sqlStr := `update user set password=? where email=?`
+	_, err := config.Db.Exec(sqlStr, password, email)
+	if err != nil {
+		fmt.Printf("update failed ,err:%v\n", err)
+		return
+	}
+}
+
+func email_exist_user(email string) bool {
+	return true
+}
+
+func username_exist_user(username string) bool {
+	return true
+}
+
+func password_match_username(username string, password string) bool {
+	return true
+}
+
+func password_match_email(email string, password string) bool {
+	return true
+}
+
+func get_userId_email(email string) int {
+	return 1
+}
+
+func get_userId_username(username string) int {
+	return 1
 }
