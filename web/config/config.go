309438781c411566bfeb3e06b2900f726487db45
Ethkuil <hobartlin23@gmail.com>
2023-11-19 23:37:52 +0800

    init db and redis

--- web/config/config.go
+++ web/config/config.go
@@ -1,5 +1,22 @@
 package config
 
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
 // InitConfig initializes the configuration of the application
 func InitConfig() {
+	viper.AutomaticEnv()
+	// We can use `redis.host` instead of `REDIS_HOST`
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Read remaining configs from file
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
+
+	viper.WatchConfig()
 }
--- web/model/mysql.go
+++ web/model/mysql.go
@@ -1,1 +1,32 @@
 package model
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+// InitDb initializes the database connection
+func InitDb() {
+	host := viper.GetString("db.host")
+	port := viper.GetString("db.port")
+	name := viper.GetString("db.name")
+	user := viper.GetString("db.user")
+	password := viper.GetString("db.password")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, name)
+	// Connect to database
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// AutoMigrateDb migrates the database
+func AutoMigrateDb() {
+	// TODO: implement
+}
