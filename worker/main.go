b132c150372b3280ff724dd2589fa9a61abe4fc1
Ethkuil <hobartlin23@gmail.com>
2023-12-27 23:22:55 +0800

    Setup `worker` architecture

--- worker/main.go
+++ worker/main.go
@@ -1,4 +1,47 @@
 package main
 
+import (
+	"log"
+	"strconv"
+
+	"github.com/THUAI-ssast/hiper-backend/web/model"
+
+	"github.com/THUAI-ssast/hiper-backend/worker/mq"
+	"github.com/THUAI-ssast/hiper-backend/worker/repository"
+	"github.com/THUAI-ssast/hiper-backend/worker/task"
+)
+
 func main() {
+	model.InitDb()
+	model.InitRedis()
+
+	for {
+		stream, err := mq.GetTask()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		values := stream.Messages[0].Values
+
+		switch stream.Stream {
+		case "build":
+			repository.UpdateBuildState(values, model.TaskStateRunning)
+			task.Build(values)
+			repository.UpdateBuildState(values, model.TaskStateFinished)
+		case "manual_match", "auto_match":
+			match_id_str := values["match_id"].(string)
+			match_id_int, err := strconv.Atoi(match_id_str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			match_id := uint(match_id_int)
+			repository.UpdateMatchState(match_id, model.TaskStateRunning)
+			task.Match(match_id)
+			repository.UpdateMatchState(match_id, model.TaskStateFinished)
+		}
+
+		if err := mq.AckTask(stream); err != nil {
+			log.Println(err)
+		}
+	}
 }
---
+++ worker/task/build.go
@@ -0,0 +1,10 @@
+package task
+
+// TODO
+func Build(values map[string]interface{}) {
+	// 获取任务所需信息
+
+	// 起容器，执行任务
+
+	// 等待任务完成，获取任务输出，保存与修改相关信息
+}
