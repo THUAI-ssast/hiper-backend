efae1e480e5a87976f80f21485cdd0ae4f262a20
Ethkuil <hobartlin23@gmail.com>
2024-01-03 23:32:55 +0800

    Add build task execution

--- worker/task/build.go
+++ worker/task/build.go
@@ -1,35 +1,76 @@
 package task
 
 import (
-	"crypto/md5"
 	"fmt"
+	"io"
 
 	"github.com/THUAI-ssast/hiper-backend/web/model"
 	"github.com/THUAI-ssast/hiper-backend/worker/repository"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 )
 
-func Build(domain repository.DomainType, id uint) (err error) {
-	repository.StartBuildTask(domain, id)
-	var taskState model.TaskState
-	var msg string
-	switch domain {
-	case repository.GameLogicDomain:
-		taskState, msg, err = buildGameLogic(id)
-	case repository.AiDomain:
-		taskState, msg, err = buildAI(id)
+func Build(domain repository.DomainType, id uint) error {
+	dockerfile, err := getDockerfile(domain, repository.BuildOperation, id)
+	if err != nil {
+		return err
 	}
-	repository.EndBuildTask(domain, id, taskState, msg)
-	return err
-}
+	if dockerfile == "" {
+		return nil
+	}
 
-// 获取任务所需信息
-// 起容器，执行任务
-// 等待任务完成，获取任务输出，保存与修改相关信息
+	repository.StartBuildTask(domain, id)
 
-func buildGameLogic(gameID uint) {
-	// TODO
-}
+	nameVersioned, err := prepareImage(repository.GameLogicDomain, repository.BuildOperation, id)
+	if err != nil {
+		repository.EndBuildTask(domain, id, model.TaskStateSystemError, err.Error())
+		return err
+	}
+	/* TODO: 限制以下几个方面，以确保安全性：
+		 * CPU、内存占用
+		 * 运行时间
+	     * 日志长度
+	*/
+	containerConfig := &container.Config{Image: nameVersioned}
+	hostConfig := &container.HostConfig{
+		Binds:      getBinds(domain, id),
+		AutoRemove: true,
+	}
+	resp, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
+	if err != nil {
+		repository.EndBuildTask(domain, id, model.TaskStateSystemError, err.Error())
+		return err
+	}
+	err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		repository.EndBuildTask(domain, id, model.TaskStateSystemError, err.Error())
+		return err
+	}
+	statusCh, _ := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	// Container has exited
+	<-statusCh
 
-func buildAI(aiID uint) (taskState model.TaskState, msg string, err error) {
-	// TODO
+	// Retrieve the container exit code
+	respInspect, err := cli.ContainerInspect(ctx, resp.ID)
+	if err != nil {
+		repository.EndBuildTask(domain, id, model.TaskStateSystemError, err.Error())
+		return err
+	}
+	exitCode := respInspect.State.ExitCode
+	// Retrieve container logs
+	logOptions := types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true}
+	logsReader, err := cli.ContainerLogs(ctx, resp.ID, logOptions)
+	if err != nil {
+		repository.EndBuildTask(domain, id, model.TaskStateSystemError, err.Error())
+		return err
+	}
+	defer logsReader.Close()
+	logs, _ := io.ReadAll(logsReader)
+
+	// Interpret the exit code and update task state accordingly
+	if exitCode != 0 {
+		repository.EndBuildTask(domain, id, model.TaskStateInputError, fmt.Sprintf("Docker container exited with non-zero exit code: %d. Logs:\n%s", exitCode, string(logs)))
+	}
+	repository.EndBuildTask(domain, id, model.TaskStateFinished, fmt.Sprintf("Docker container exited successfully. Logs:\n%s", string(logs)))
+	return nil
 }
--- worker/task/image.go
+++ worker/task/image.go
@@ -3,12 +3,63 @@ package task
 import (
 	"archive/tar"
 	"bytes"
+	"crypto/md5"
+	"fmt"
 	"io"
 
 	"github.com/THUAI-ssast/hiper-backend/web/model"
 	"github.com/THUAI-ssast/hiper-backend/worker/repository"
 	"github.com/docker/docker/api/types"
 )
+
+func getDockerfile(domain repository.DomainType, operation repository.OperationType, id uint) (dockerfile string, err error) {
+	switch domain {
+	case repository.GameLogicDomain:
+		var g model.Game
+		g, err = model.GetGameByID(id, fmt.Sprintf("game_logic_%s_dockerfile", operation))
+		if err != nil {
+			return
+		}
+		switch operation {
+		case repository.BuildOperation:
+			dockerfile = g.GameLogic.Build.Dockerfile
+		case repository.RunOperation:
+			dockerfile = g.GameLogic.Run.Dockerfile
+		}
+	case repository.AiDomain:
+		var ai model.Ai
+		ai, err = model.GetAiByID(id, true)
+		if err != nil {
+			return
+		}
+		switch operation {
+		case repository.BuildOperation:
+			dockerfile = ai.Sdk.BuildAi.Dockerfile
+		case repository.RunOperation:
+			dockerfile = ai.Sdk.RunAi.Dockerfile
+		}
+	}
+	return
+}
+
+func prepareImage(domain repository.DomainType, operation repository.OperationType, id uint) (string, error) {
+	dockerfile, err := getDockerfile(domain, operation, id)
+	if err != nil {
+		return "", err
+	}
+	dockerfileHash := md5.Sum([]byte(dockerfile))
+	name := fmt.Sprintf("%s-%d-%s", domain, id, operation)
+	tag := fmt.Sprintf("%x", dockerfileHash)
+	nameVersioned := fmt.Sprintf("%s:%x", name, dockerfileHash)
+	// Check if image exists
+	if _, _, err := cli.ImageInspectWithRaw(ctx, nameVersioned); err != nil {
+		// image not exists, build it
+		if err = buildImage(dockerfile, name, tag, domain, operation, id); err != nil {
+			return "", err
+		}
+	}
+	return nameVersioned, nil
+}
 
 // param name and tag: `--tag <name>:<tag>`
 func buildImage(dockerfileContent, name string, tag string, domain repository.DomainType, operation repository.OperationType, id uint) error {
